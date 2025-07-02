1b1827a22c07e2aae66fe3cfce64a9fcd2944abd
vidishadixit <vidishadixit43@gmail.com>
2025-07-02 21:32:12 +0530

    Group Concat String

---
+++ Group Concat String.sql
@@ -0,0 +1,74 @@
+/*
+Write a solution to find for each date the number of different products sold and their names.
+
+The sold products names for each date should be sorted lexicographically.
+
+Return the result table ordered by sell_date.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Activities table:
++------------+------------+
+| sell_date  | product     |
++------------+------------+
+| 2020-05-30 | Headphone  |
+| 2020-06-01 | Pencil     |
+| 2020-06-02 | Mask       |
+| 2020-05-30 | Basketball |
+| 2020-06-01 | Bible      |
+| 2020-06-02 | Mask       |
+| 2020-05-30 | T-Shirt    |
++------------+------------+
+Output: 
++------------+----------+------------------------------+
+| sell_date  | num_sold | products                     |
++------------+----------+------------------------------+
+| 2020-05-30 | 3        | Basketball,Headphone,T-shirt |
+| 2020-06-01 | 2        | Bible,Pencil                 |
+| 2020-06-02 | 1        | Mask                         |
++------------+----------+------------------------------+
+Explanation: 
+For 2020-05-30, Sold items were (Headphone, Basketball, T-shirt), we sort them lexicographically and separate them by a comma.
+For 2020-06-01, Sold items were (Pencil, Bible), we sort them lexicographically and separate them by a comma.
+For 2020-06-02, the Sold item is (Mask), we just return it.
+*/
+
+
+-- mysql
+select
+	sell_date,
+	count(distinct product) as num_sold,
+	group_concat(distinct product order by product) as products
+from
+	activities
+group by 
+	sell_date
+order by
+	sell_date
+
+
+--SQL Server
+
+with distProduct as
+(
+	select
+		sell_date,
+		distinct product
+	from
+		activities
+	)
+select
+	sell_date,
+	count(product) as num_sold,
+	STRING_AGG(product,',') within group (order by product) as products
+from
+	distProduct
+group by
+	sell_date
+order by 
+	sell_date
